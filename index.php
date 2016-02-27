91280b8a2aee27a6aeace5b21e9ad01efe8d47d0
Jordie Ashton <jordie@trendhop.net>
2016-02-27 16:05:25 +0000

    Starting again, changed file names, removed files

--- index.php
+++ index.php
@@ -4,36 +4,33 @@
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/reset.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/reset.css"  media="screen"/>
+      <link type="text/css" rel="stylesheet" href="css/trendhop_main.css"  media="screen"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen"/>
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <!--[if lte IE 9]>
+        <div style="background: #ffebee; padding: 20px;">
+          <p>You are using an outdated browser. Our website might not work well with your browser and your transactions might be less secure.<br/>We      recommend getting a new browser like Google Chrome.<br/><a href="https://www.google.com/chrome/browser" target="_blank">Click here to download      Google Chrome</a></p>
+        </div>    
+      <![endif]-->
     </head>
 
     <body>
-    
-    <ul id="dropdown1" class="dropdown-content">
-      <li><a href="#!">one</a></li>
-      <li><a href="#!">two</a></li>
-      <li class="divider"></li>
-      <li><a href="#!">three</a></li>
-    </ul>
-        
+    <div class="navbar-fixed">
       <nav>
         <div class="nav-wrapper white">
           <a href="index.php" class='brand-logo'><img src="img/logo.png" class="logo" alt="TrendHop"></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="index.php">home</a></li>
-            <li><a href="about.php">about</a></li>
-            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
-          </ul>
-
         </div>
       </nav>
+    </div> 
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript">
+        $(".dropdown-button").dropdown(hover: false);
+      </script>
     </body>
   </html>
