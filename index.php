c1fbc4510d19c8021999adafeef5af41b7fee234
Jordie Ashton <jordie@trendhop.net>
2016-02-27 15:07:27 +0000

    Added images

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/reset.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
 
       <!--Let browser know website is optimized for mobile-->
@@ -13,27 +13,24 @@
     </head>
 
     <body>
-
+    
+    <ul id="dropdown1" class="dropdown-content">
+      <li><a href="#!">one</a></li>
+      <li><a href="#!">two</a></li>
+      <li class="divider"></li>
+      <li><a href="#!">three</a></li>
+    </ul>
         
-        <div class="row header-row">
-          <div class="col s12 header">
-            <div class="col s6"><a href="index.php"><img src="img/logo-new-colour.png" alt="TrendHop" class="logo"></a></div>
-            <div class="col s6 right-align">Social</div>
-          </div>
+      <nav>
+        <div class="nav-wrapper white">
+          <a href="index.php" class='brand-logo'><img src="img/logo.png" class="logo" alt="TrendHop"></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="index.php">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Categories<i class="material-icons right">arrow_drop_down</i></a></li>
+          </ul>
         </div>
-        <div class="row">
-          <div class="col s12 navigation red darken-3">
-            <div class="col s2 tab">
-              <a href="index.php">Home</a>
-            </div>
-            <div class="col s2 tab">
-              <a href="index.php">About</a>
-            </div>
-            <div class="col s2 tab">
-              <a href="index.php">Categories</a>
-            </div>
-          </div>
-        </div>
+      </nav>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
