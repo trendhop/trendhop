14f86491bb0a348a168f6eafb6fd5b63ccfa7982
Jordie Ashton <jordie@trendhop.net>
2016-03-01 01:07:31 +0000

    Updated Files

--- index.php
+++ index.php
@@ -70,9 +70,6 @@
           </div>
         </div>
       </header>
-      <div class="content">
-        
-      </div>
       <div class="row main_content">
         <div class="col s9 main_content">
           <div class="slider">
@@ -114,31 +111,41 @@
 
         <div class="trending">
           <h3>Trending!</h3>
-          <div class="row">
-            <div class="col s9">
-              <div class="col s6">
-                <div class="trend">
-                  <img src="image/article_image/ainsleybanister.jpg" class="trend_image">
-                </div>
-              </div>
+          <?php 
+              $query = "SELECT * FROM articles WHERE article_status = 'published' LIMIT 9";
+              $selectQuery = mysqli_query($connection, $query);
 
-              <div class="col s3">
-                
-              </div>
-            </div>
-          </div>
+              while($row = mysqli_fetch_assoc($selectQuery)) {
+                $fresh_image = $row['article_img'];
+                $fresh_category = $row['article_cat_id'];
+                $fresh_author = $row['article_author'];
+                $fresh_title = $row['article_title'];
+                $fresh_date = date('d/m/Y', strtotime($row['article_date']));
 
-          <div class="row">
-            <div class="col s9">
-              <div class="col s6">
-                <div class="trend">
-                  <img src="image/article_image/ainsleybanister.jpg" class="trend_image">
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                echo "<div class='row'>";
+                      echo "<div class='col s9'>";
+                      echo "<div class='col s6'>";
+                      echo "<img src='image/article_image/$fresh_image' class='trend_image' alt='$fresh_image'>";
+                      echo "</div>";
+                      $query = "SELECT * FROM categories WHERE id = $fresh_category";
+
+                      $select_cat_id = mysqli_query($connection, $query);
+
+                      while($row = mysqli_fetch_assoc($select_cat_id)) {
+                        $cat_id = $row['id'];
+                        $cat_title = $row['cat_title'];
+
+                        echo "<p class='trend_category'>$cat_title</p>";
+                      }
+                      echo "<p class='trend_author'>$fresh_author</p>";
+                      echo "<p class='trend_title'><a href='#'>$fresh_title</a></p>";
+                      echo "<p class='trend_date'>$fresh_date</p>";
+                echo "</div>";
+                echo "</div>";
+              }
+            ?>
+         </div>
+       </div>
 
         <div class="col s3 grey">
           Sidebar
